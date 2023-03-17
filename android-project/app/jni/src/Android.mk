6fab64759bd7400c78281e2fb01f4d2dfb742a9e
captain1947 <sayantandebin@gmail.com>
2023-03-17 09:07:04 +0530

    update

--- android-project/app/jni/src/Android.mk
+++ android-project/app/jni/src/Android.mk
@@ -7,12 +7,14 @@ LOCAL_MODULE := main
 SRC_PATH := ../../../../src
 SDL_PATH := ../SDL
 SDL_IMAGE_PATH := ../SDL2_image
+SDL_MIXER_PATH := ../SDL2_mixer
 SDL_TTF_PATH := ../SDL2_ttf
 SDL_NET_PATH := ../SDL2_net
 
 LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/$(SDL_PATH)/include \
 	$(LOCAL_PATH)/$(SDL_IMAGE_PATH)/include \
+	$(LOCAL_PATH)/$(SDL_MIXER_PATH)/include \
 	$(LOCAL_PATH)/$(SDL_TTF_PATH)/include \
 	$(LOCAL_PATH)/$(SDL_NET_PATH)/include \
 	$(LOCAL_PATH)/$(SRC_PATH) \
@@ -21,7 +23,7 @@ LOCAL_SRC_FILES :=  \
 	../../../../src/main.cpp \
 	../../../../src/engine.cpp
 
-LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_ttf SDL2_net
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer SDL2_ttf SDL2_net
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -lOpenSLES -llog -landroid
 
