4f016a2129f959411085d8161d2f40a182c108d6
CAPTAIN1947 <sayantandebin@gmail.com>
2023-05-17 18:55:43 +0530

    update

--- android-project/app/jni/src/Android.mk
+++ android-project/app/jni/src/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := main
 
-SRC_PATH := ../../../../src
+SRC_PATH := .
 SDL_PATH := ../SDL
 SDL_IMAGE_PATH := ../SDL2_image
 SDL_MIXER_PATH := ../SDL2_mixer
@@ -12,18 +12,18 @@ SDL_TTF_PATH := ../SDL2_ttf
 SDL_NET_PATH := ../SDL2_net
 
 LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/../headers \
 	$(LOCAL_PATH)/$(SDL_PATH)/include \
 	$(LOCAL_PATH)/$(SDL_IMAGE_PATH)/include \
 	$(LOCAL_PATH)/$(SDL_MIXER_PATH)/include \
 	$(LOCAL_PATH)/$(SDL_TTF_PATH)/include \
 	$(LOCAL_PATH)/$(SDL_NET_PATH)/include \
+	$(LOCAL_PATH)/../supernova/src \
 	$(LOCAL_PATH)/$(SRC_PATH) \
 
-LOCAL_SRC_FILES :=  \
-	../../../../src/main.cpp \
-	../../../../src/engine.cpp
+LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/$(SRC_PATH)/*.cpp)
 
-LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer SDL2_ttf SDL2_net
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer SDL2_ttf SDL2_net supernova
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -lOpenSLES -llog -landroid
 
